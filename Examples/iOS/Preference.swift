2a98f18bd39b8ed8fbe87d3e6f11667e5ce994b8
jognheum park <jongheum.park@inisoft.co.kr>
2021-03-23 11:11:04 +0900

    Pereference: Update Preference: Add keyframe configure

--- Examples/iOS/Preference.swift
+++ Examples/iOS/Preference.swift
@@ -1,6 +1,10 @@
 struct Preference {
     static var defaultInstance = Preference()
 
-    var uri: String? = "rtmp://192.168.1.12/live"
+    var uri: String? = "rtmp://192.168.10.174/live"
     var streamName: String? = "live"
+    var keyframeInterval: Int? = 30
+    var keyframeDuration: Double? = 1.0
+    var resolution: Int? = 2
+    var allowBFrame: Bool? = true
 }
--- Examples/iOS/PreferenceViewController.swift
+++ Examples/iOS/PreferenceViewController.swift
@@ -3,17 +3,38 @@ import UIKit
 final class PreferenceViewController: UIViewController {
     @IBOutlet private weak var urlField: UITextField?
     @IBOutlet private weak var streamNameField: UITextField?
-
+    @IBOutlet private weak var keyFrameIntervalField: UITextField?
+    @IBOutlet private weak var keyFrameDurationField: UITextField?
+    @IBOutlet private weak var captureResolutionSegmentControl: UISegmentedControl?
+    @IBOutlet private weak var bFrameAllowSwitch: UISwitch!
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         urlField?.text = Preference.defaultInstance.uri
         streamNameField?.text = Preference.defaultInstance.streamName
+        keyFrameIntervalField?.text = "\(Preference.defaultInstance.keyframeInterval!)"
+        keyFrameDurationField?.text = "\(Preference.defaultInstance.keyframeDuration!)"
+        captureResolutionSegmentControl?.selectedSegmentIndex = Preference.defaultInstance.resolution!
+        bFrameAllowSwitch?.isOn = Preference.defaultInstance.allowBFrame!
     }
 
     @IBAction func on(open: UIButton) {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let controller: UIViewController = storyboard.instantiateViewController(withIdentifier: "PopUpLive")
         present(controller, animated: true, completion: nil)
+    }
+    
+    @IBAction func resolutionValueChanged(_ sender: Any) {
+        guard let resolutionSegment = sender as? UISegmentedControl else {
+            return
+        }
+        Preference.defaultInstance.resolution = resolutionSegment.selectedSegmentIndex
+    }
+    @IBAction func bFrameAllowSwitchValueChnged(_ sender: Any) {
+        guard let bFrameAllowSwitch = sender as? UISwitch else {
+            return
+        }
+        Preference.defaultInstance.allowBFrame = bFrameAllowSwitch.isOn
     }
 }
 
@@ -24,6 +45,14 @@ extension PreferenceViewController: UITextFieldDelegate {
         }
         if streamNameField == textField {
             Preference.defaultInstance.streamName = textField.text
+        }
+        if keyFrameIntervalField == textField {
+            Preference.defaultInstance.keyframeInterval = Int(textField.text!)
+            print("\(Preference.defaultInstance.keyframeInterval)")
+        }
+        if keyFrameDurationField == textField {
+            Preference.defaultInstance.keyframeDuration = Double(textField.text!)
+            print("\(Preference.defaultInstance.keyframeDuration)")
         }
         textField.resignFirstResponder()
         return true
